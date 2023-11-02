74a14aa7770f3053a76d63a705113fe9093af7cd
Giorgio Gamba <giorgiogamba97@gmail.com>
2023-11-02 20:56:00 +0100

    add texture creation code

--- GameEngine/main.cpp
+++ GameEngine/main.cpp
@@ -122,6 +122,39 @@ bool loadShaders(GLuint& program)
 
 }
 
+GLuint CreateTexture()
+{
+    // TEXTURE 1
+    // Texture Initialization
+    int texwidth, textheight;
+    unsigned char* teximage = SOIL_load_image("Textures/cat.png", &texwidth, &textheight, NULL, SOIL_LOAD_RGBA);
+
+    GLuint tex;
+    glGenTextures(1, &tex);
+    glBindTexture(GL_TEXTURE_2D, tex);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+    if (teximage)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texwidth, textheight, 0, GL_RGBA, GL_UNSIGNED_BYTE, teximage);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "ERROR: error while loading texture" << std::endl;
+        return 0;
+    }
+
+    glActiveTexture(0);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    SOIL_free_image_data(teximage);
+
+    return tex;
+}
 int main(void)
 {
     GLFWwindow* window;
