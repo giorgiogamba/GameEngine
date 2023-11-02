834a9bf07db68b06adbe044ea32082d7812700eb
Giorgio Gamba <giorgiogamba97@gmail.com>
2023-11-02 21:01:05 +0100

    add quad with indices definition

--- GameEngine/main.cpp
+++ GameEngine/main.cpp
@@ -22,6 +22,30 @@
 #include "src/Public/Utils.h"
 
 #pragma endregion
+
+#pragma region Triangle Vertices
+
+// Basic 2 triangles definition
+Vertex vertices[] =
+{
+    glm::vec3(-0.5f, 0.5f, 0.0f),       glm::vec3(1.0f, 0.0f, 0.0f),    glm::vec2(0.0f, 1.0f), glm::vec3(0.f, 0.f, -1.f),
+    glm::vec3(-0.5f, -0.5f, 0.0f),      glm::vec3(0.0f, 1.0f, 0.0f),    glm::vec2(0.0f, 0.0f), glm::vec3(0.f, 0.f, -1.f),
+    glm::vec3(0.5f, -0.5f, 0.0f),       glm::vec3(0.0f, 0.0f, 1.0f),    glm::vec2(1.0f, 0.0f), glm::vec3(0.f, 0.f, -1.f),
+    glm::vec3(0.5f, 0.5f, 0.0f),        glm::vec3(1.0f, 1.0f, 0.0f),    glm::vec2(1.0f, 1.0f), glm::vec3(0.f, 0.f, -1.f),
+};
+
+unsigned numVertices = sizeof(vertices) / sizeof(Vertex);
+
+GLuint indices[] =
+{
+    0, 1, 2,
+    0, 2, 3
+};
+
+unsigned numIndices = 6;
+
+#pragma endregion
+
 bool loadShaders(GLuint& program)
 {
     char infoLog[512];
