30db1e82ba84325b64602fe64c52874a24d47c34
Giorgio Gamba <giorgiogamba97@gmail.com>
2023-11-05 12:29:03 +0100

    fix vertex shader error

--- GameEngine/shaders/vertex_shader.glsl
+++ GameEngine/shaders/vertex_shader.glsl
@@ -2,7 +2,7 @@
 
 layout (location = 0) in vec3 vertex_position;
 layout (location = 1) in vec3 vertex_color;
-layout (location = 2) in vec3 vertex_texcoord;
+layout (location = 2) in vec2 vertex_texcoord;
 layout (location = 3) in vec3 vertex_normal;
 
 out vec3 vs_position;
