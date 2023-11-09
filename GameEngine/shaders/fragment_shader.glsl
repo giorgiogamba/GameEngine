5e97ae130c81d17d3f2161c8a975fff50fc13a74
Giorgio Gamba <giorgiogamba97@gmail.com>
2023-11-09 09:14:11 +0100

    some fragment shader fixes

--- GameEngine/shaders/fragment_shader.glsl
+++ GameEngine/shaders/fragment_shader.glsl
@@ -29,18 +29,16 @@ vec3 computeDiffuseLightning()
 
 vec3 computeSpecularLightning()
 {
-	vec3 lightToPosDir = normalize(vec3(vs_position - lightPos));
-	vec3 reflectLightToPosDir = reflect(lightToPosDir, vs_normal);
+	vec3 lightToPosDir = normalize(vs_position - lightPos);
+	vec3 reflectLightToPosDir = normalize(reflect(lightToPosDir, normalize(vs_normal)));
 	vec3 posToViewDir = normalize(vec3(cameraPos - vs_position));
-	float specularConstant = pow(max(dot(posToViewDir, reflectLightToPosDir), 0), 40);
-	return vec3(1.f, 1.f, 1.f) * specularConstant;
+	float specularConstant = pow(max(dot(posToViewDir, reflectLightToPosDir), 0), 35);
+	return material.specularColor * specularConstant;
 }
 
 void main()
 {
 	fs_color = texture(texture, vs_texcoord)
 			 * vec4(vs_color, 1.f)
-			 * (vec4(material.ambientColor, 1.f)
-				+ vec4(computeDiffuseLightning(), 1.f)
-				+ vec4(computeSpecularLightning(), 1.f));
+			 * (vec4(material.ambientColor, 1.f) + vec4(computeDiffuseLightning(), 1.f) + vec4(computeSpecularLightning(), 1.f));
 }
