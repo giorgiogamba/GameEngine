f314462ec470721461137e059be0c0679b584051
Giorgio Gamba <giorgiogamba97@gmail.com>
2023-11-18 15:29:22 +0100

    add Triangle class

--- GameEngine/src/Public/Primitive.h
+++ GameEngine/src/Public/Primitive.h
@@ -35,6 +35,35 @@ private:
 
 };
 
+class Triangle : public Primitive
+{
+public:
+
+	Triangle() : Primitive()
+	{
+		// Basic 2 triangles definition
+		Vertex vertices[] =
+		{
+			glm::vec3(-0.5f, 0.5f, 0.0f),       glm::vec3(1.0f, 0.0f, 0.0f),    glm::vec2(0.0f, 1.0f), glm::vec3(0.f, 0.f, 1.f),
+			glm::vec3(-0.5f, -0.5f, 0.0f),      glm::vec3(0.0f, 1.0f, 0.0f),    glm::vec2(0.0f, 0.0f), glm::vec3(0.f, 0.f, 1.f),
+			glm::vec3(0.5f, -0.5f, 0.0f),       glm::vec3(0.0f, 0.0f, 1.0f),    glm::vec2(1.0f, 0.0f), glm::vec3(0.f, 0.f, 1.f),
+			glm::vec3(0.5f, 0.5f, 0.0f),        glm::vec3(1.0f, 1.0f, 0.0f),    glm::vec2(1.0f, 1.0f), glm::vec3(0.f, 0.f, 1.f),
+		};
+
+		unsigned numVertices = sizeof(vertices) / sizeof(Vertex);
+
+		GLuint indices[] =
+		{
+			0, 1, 2,
+		};
+
+		unsigned numIndices = 6;
+
+		Initialize(vertices, numVertices, indices, numIndices);
+	}
+
+};
+
 class Quad : public Primitive
 {
 public:
