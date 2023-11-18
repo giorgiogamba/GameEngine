4369a247d01b44e8dbf0f1e153343c6078fa5544
Giorgio Gamba <giorgiogamba97@gmail.com>
2023-11-18 15:19:31 +0100

    create Primitive and Quad class

---
+++ GameEngine/src/Public/Primitive.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <vector>
+
+class Primitive
+{
+public:
+
+	Primitive() {}
+	~Primitive() {}
+
+	void Initialize(Vertex* vertices, const unsigned NumVertices, GLuint* indices, const unsigned NumIndices)
+	{
+		for (size_t i = 0; i < NumVertices; i++)
+		{
+			this->vertices.push_back(vertices[i]);
+		}
+
+		for (size_t i = 0; i < NumIndices; i++)
+		{
+			this->indices.push_back(indices[i]);
+		}
+	}
+
+	Vertex* GetVertices() { return this->vertices.data(); }
+	GLuint* GetIndices() { return this->indices.data(); }
+
+	int GetNumVertices() const { return this->vertices.size(); }
+	int GetNumIndices() const { return this->indices.size(); }
+
+private:
+
+	std::vector<Vertex> vertices;
+	std::vector<GLuint> indices;
+
+};
+
+class Quad : public Primitive
+{
+public:
+
+	Quad() : Primitive()
+	{
+		// Basic 2 triangles definition
+		Vertex vertices[] =
+		{
+			glm::vec3(-0.5f, 0.5f, 0.0f),       glm::vec3(1.0f, 0.0f, 0.0f),    glm::vec2(0.0f, 1.0f), glm::vec3(0.f, 0.f, 1.f),
+			glm::vec3(-0.5f, -0.5f, 0.0f),      glm::vec3(0.0f, 1.0f, 0.0f),    glm::vec2(0.0f, 0.0f), glm::vec3(0.f, 0.f, 1.f),
+			glm::vec3(0.5f, -0.5f, 0.0f),       glm::vec3(0.0f, 0.0f, 1.0f),    glm::vec2(1.0f, 0.0f), glm::vec3(0.f, 0.f, 1.f),
+			glm::vec3(0.5f, 0.5f, 0.0f),        glm::vec3(1.0f, 1.0f, 0.0f),    glm::vec2(1.0f, 1.0f), glm::vec3(0.f, 0.f, 1.f),
+		};
+
+		unsigned numVertices = sizeof(vertices) / sizeof(Vertex);
+
+		GLuint indices[] =
+		{
+			0, 1, 2,
+			0, 2, 3
+		};
+
+		unsigned numIndices = 6;
+
+		Initialize(vertices, numVertices, indices, numIndices);
+	}
+
+};
+
+
+
+
+
