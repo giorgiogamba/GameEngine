ec775e0ef6c0d5d9c095e4ba27246ca9d36082f3
Giorgio Gamba <giorgiogamba97@gmail.com>
2023-12-09 22:57:45 +0100

    disable mesh position update

--- GameEngine/src/Public/Mesh.h
+++ GameEngine/src/Public/Mesh.h
@@ -47,7 +47,7 @@ public:
 		if (!window || !shader)
 			return;
 
-		UpdateInput(window);
+		//UpdateInput(window);
 		CreateModelMatrix();
 		shader->AddUniformMatrix4fv(modelMatrix, "ModelMatrix");
 	}
@@ -94,36 +94,35 @@ private:
 		glBindVertexArray(0);
 	}
 
-	void UpdateInput(GLFWwindow* window)
-	{
-		if (!window)
-			return;
-
-		if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-		{
-			position.z -= 0.005f;
-		}
-		if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-		{
-			position.z += 0.005f;
-		}
-		if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-		{
-			position.x -= 0.005f;
-		}
-		if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-		{
-			position.x += 0.005f;
-		}
-		if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
-		{
-			rotation.y -= 0.05f;
-		}
-		if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
-		{
-			rotation.y += 0.05f;
-		}
-	}
+	//void UpdateInput(GLFWwindow* window)
+	//{
+	//	if (!window)
+	//		return;
+	//	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+	//	{
+	//		position.z -= 0.005f;
+	//	}
+	//	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+	//	{
+	//		position.z += 0.005f;
+	//	}
+	//	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+	//	{
+	//		position.x -= 0.005f;
+	//	}
+	//	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+	//	{
+	//		position.x += 0.005f;
+	//	}
+	//	if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+	//	{
+	//		rotation.y -= 0.05f;
+	//	}
+	//	if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+	//	{
+	//		rotation.y += 0.05f;
+	//	}
+	//}
 
 	void InitVAO()
 	{
